f152668688ca7fa5d5682fcddcbbfc58e9772609
Max Malkin <maxim_malkin@outlook.com>
2025-09-29 21:54:37 -0600

    lint

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import "dotenv/config";
+import 'dotenv/config';
 
 export interface DatabaseConfig {
 	host: string;
@@ -9,9 +9,9 @@ export interface DatabaseConfig {
 }
 
 export const dbConfig: DatabaseConfig = {
-	host: process.env.DB_HOST || "127.0.0.1",
-	port: Number.parseInt(process.env.DB_PORT || "5432", 10),
-	user: process.env.DB_USER || "username",
-	password: process.env.DB_PASSWORD || "password",
-	database: process.env.DB_NAME || "planty",
+	host: process.env.DB_HOST || '127.0.0.1',
+	port: Number.parseInt(process.env.DB_PORT || '5432', 10),
+	user: process.env.DB_USER || 'username',
+	password: process.env.DB_PASSWORD || 'password',
+	database: process.env.DB_NAME || 'planty',
 };
