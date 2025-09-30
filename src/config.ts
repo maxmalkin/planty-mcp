927876273cb37f9a3a411f689085e4752a18286f
Max Malkin <maxim_malkin@outlook.com>
2025-09-29 20:59:23 -0600

    Add @types/pg and pg dependencies; remove better-sqlite3 dependencies

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+export interface DatabaseConfig {
+	host: string;
+	port: number;
+	user: string;
+	password: string;
+	database: string;
+}
+
+export const dbConfig: DatabaseConfig = {
+	host: 'process.env.DB_HOST || "localhost"',
+	port: parseInt(process.env.DB_PORT || "5432", 10),
+	user: process.env.DB_USER || "username",
+	password: process.env.DB_PASSWORD || "password",
+	database: process.env.DB_NAME || "planty",
+};
